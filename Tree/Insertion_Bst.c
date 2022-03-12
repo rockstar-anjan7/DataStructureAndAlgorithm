2d48aef9bb64b55ae62380721875b9a4bab98deb
anjan <vishwakarmaanjan@gmail.com>
2022-03-12 17:31:20 +0530

    adding new files

---
+++ Tree/Insertion_Bst.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n;                                 // creating a node pointer
+    n = (struct node *)malloc(sizeof(struct node)); // Allocating memory in the heap
+    n->data = data;                                 // Setting the data
+    n->left = NULL;                                 // Setting the left and right children to NULL
+    n->right = NULL;                                // Setting the left and right children to NULL
+    return n;                                       // Finally returning the created node
+}
+void inOrder(struct node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+ 
+void insert(struct node *root, int key)
+{
+    struct node *prev = NULL;
+    while (root != NULL)
+    {
+        prev = root;
+        if (key == root->data)
+        {
+            printf("Cannot insert %d, already in BST", key);
+            return;
+        }
+        else if (key < root->data)
+        {
+            root = root->left;
+        }
+        else
+        {
+            root = root->right;
+        }
+    }
+    struct node *new = createNode(key);
+    if (key < prev->data)
+    {
+        prev->left = new;
+    }
+    else
+    {
+        prev->right = new;
+    }
+}
+
+int main()
+{
+
+    // Constructing the root node - Using Function (Recommended)
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+    // Finally The tree looks like this:
+    //      5
+    //     / \  note:agar root 3 par hai toh prev 5 par hoga.
+    //    3   6   ya agar root 1 par hai toh prev 3 par hoga.
+    //   / \
+    //  1   4
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    inOrder(p);
+    printf("\n");
+    insert(p, 16);
+    insert(p, 0);
+    insert(p, 2);
+    // printf("Data is inserted %d \n", p->right->right->data);
+    inOrder(p);
+
+    return 0;
+}
